651d539b2392d824c0730ce4e3f58282f9440547
Nico <nico@brinkkemper.nl>
2019-03-18 22:25:31 +0100

    ignore

--- tools/kubectl/configure.sh
+++ tools/kubectl/configure.sh
@@ -4,7 +4,7 @@ set -e
 
 gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
 gcloud config set project handlefy
-gcloud container clusters get-credentials $K8S_CLUSTER --zone $K8S_ZONE
+gcloud container clusters get-credentials "$K8S_CLUSTER" --zone "$K8S_ZONE"
 
 kubectl create clusterrolebinding cluster-admin-binding \
   --clusterrole=cluster-admin \
