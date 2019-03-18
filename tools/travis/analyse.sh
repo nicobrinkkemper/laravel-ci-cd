d6ba5fc2d423e120470d051dac0d5c196b5e9ec2
Nico <nico@brinkkemper.nl>
2019-03-18 05:56:43 +0100

    ignore

--- tools/travis/analyse.sh
+++ tools/travis/analyse.sh
@@ -2,6 +2,6 @@
 
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin https://index.docker.io/v1;
-docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container phpcs && \
-docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container phpmd && \
-docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container phpstan
+docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container ./vendor/bin/phpcs && \
+docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container ./vendor/bin/phpmd && \
+docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container ./vendor/bin/phpstan
--- tools/travis/unit-tests.sh
+++ tools/travis/unit-tests.sh
@@ -2,4 +2,4 @@
 
 set -e
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin https://index.docker.io/v1;
-docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container phpunit
+docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container ./vendor/bin/phpunit
