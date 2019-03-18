38c8a060c6a258f8931330592ace6f0306591ac3
Nico <nico@brinkkemper.nl>
2019-03-18 05:42:56 +0100

    ignore

--- tools/travis/analyse.sh
+++ tools/travis/analyse.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 set -e
-
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD" && \
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin https://index.docker.io/v1;
 docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container phpcs && \
 docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container phpmd && \
 docker run --rm -i "$DOCKER_USERNAME"/"$DOCKER_REPO":"$TRAVIS_COMMIT" container phpstan
